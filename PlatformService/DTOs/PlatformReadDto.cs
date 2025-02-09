5fcd17d27191620d34f4228cf7c3d15267d80e52
Do Nhat <dmnhat.pt@gmail.com>
2025-02-09 10:53:22 +0700

    Create DTOs.

---
+++ PlatformService/DTOs/PlatformReadDto.cs
@@ -0,0 +1,9 @@
+namespace PlatformService.DTOs;
+
+public class PlatformReadDto
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Publisher { get; set; }
+    public required string Cost { get; set; }
+}
